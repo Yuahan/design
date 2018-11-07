53b7730a4febd31590c2a0881d456a3f619ffd12
yuhan <44660036+Yuahan@users.noreply.github.com>
2018-11-07 20:49:06 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -169,20 +169,20 @@
 	
 	};
 
-	// //  Category
+	//  Category
 	
- //    $('.toggles button').click(function(){
- //      var get_id = this.id;
- //      var get_current = $('.posts .' + get_id);
+    $('.toggles button').click(function(){
+      var get_id = this.id;
+      var get_current = $('.posts .' + get_id);
   
- //        $('.post').not( get_current ).hide(500);
- //        get_current.show(500);
- //    });
+        $('.post').not( get_current ).hide(500);
+        get_current.show(500);
+    });
     
     
- //    $('#showall').click(function() {
- //        $('.post').show(500);
- //    });
+    $('#showall').click(function() {
+        $('.post').show(500);
+    });
 
 
 	// Loading page
@@ -208,6 +208,7 @@
 		goToTop();
 		loaderPage();
 	});
+
 
 
 }());
